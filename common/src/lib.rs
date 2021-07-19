93e6d26e8777fd7efab96c14281cced7b873dd20
Alan Hanson <alan@oxidecomputer.com>
2021-07-19 16:53:41 -0700

    Moved RegionDefinition and RegionOptions to common
    Moved the RegionDefinition and RegionOptions that were part of the
    downstairs region.rs file to a common location.  Doing this means that
    the upstairs can then make use of them instead of having its own copy,
    which I also removed as part of this.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -8,6 +8,9 @@ use anyhow::{anyhow, bail, Context, Result};
 use serde::{Deserialize, Serialize};
 use tempfile::NamedTempFile;
 
+mod region;
+pub use region::{RegionDefinition, RegionOptions};
+
 pub fn read_json_maybe<P, T>(file: P) -> Result<Option<T>>
 where
     P: AsRef<Path>,
