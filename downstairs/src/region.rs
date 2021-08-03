ddfa216601d0494088621c7dc6dce66391b491d0
Alan Hanson <alan@oxidecomputer.com>
2021-08-03 11:41:21 -0700

    cargo fmt updates

--- downstairs/src/region.rs
+++ downstairs/src/region.rs
@@ -521,15 +521,16 @@ impl Region {
     }
 
     pub fn versions(&self) -> Vec<u64> {
-        let mut ver = self.extents
-                .iter()
-                .map(|e| e.flush_number())
-                .collect::<Vec<_>>();
+        let mut ver = self
+            .extents
+            .iter()
+            .map(|e| e.flush_number())
+            .collect::<Vec<_>>();
 
         if ver.len() > 12 {
             ver = ver[0..12].to_vec();
         }
-        println!( "Current flush_numbers [0..12]: {:?}", ver);
+        println!("Current flush_numbers [0..12]: {:?}", ver);
 
         self.extents
             .iter()
