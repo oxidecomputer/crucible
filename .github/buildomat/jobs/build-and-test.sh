b2c18ddb67b60bdf0e1e40faa84ee77a77f2caa6
Joshua M. Clulow <jmc@oxide.computer>
2021-10-04 20:06:05 -0700

    buildomat: add basic build/test job

---
+++ .github/buildomat/jobs/build-and-test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#:
+#: name = "build-and-test"
+#: variety = "basic"
+#: target = "helios"
+#: rust_toolchain = "nightly"
+#: output_rules = []
+#:
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+banner build
+ptime -m cargo build --verbose
+
+banner test
+ptime -m cargo test --lib --verbose
