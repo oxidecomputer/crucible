8fab16cdaa298b89887265775441b797a980b27e
James MacMahon <james@oxide.computer>
2021-12-17 13:59:28 -0500

    why are we creating lib.rs anyway

--- downstairs/src/lib.rs
+++ downstairs/src/lib.rs
@@ -1,4 +1,11 @@
 // Copyright 2021 Oxide Computer Company
+
+/*
+ * Note: This lib.rs was created to facilitate criterion benchmarks, which
+ * cannot benchmark binary crates. Including a minimal number of uses here
+ * allows us to write those microbenchmarks.
+ */
+
 use anyhow::{bail, Result};
 use std::collections::HashMap;
 use std::path::PathBuf;
