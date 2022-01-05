dbddda0f6d5576c7347a58ff71ee255ddf2e6ef0
Alan Hanson <alan@oxidecomputer.com>
2022-01-05 12:42:04 -0800

    Simplify

--- tools/perfdw.d
+++ tools/perfdw.d
@@ -9,87 +9,36 @@
 cdt*:::gw_read_submit_start,
 cdt*:::gw_write_submit_start,
 cdt*:::gw_flush_submit_start
-/arg1 == 0/
 {
-    start0[arg0] = timestamp;
-}
-cdt*:::gw_read_submit_start,
-cdt*:::gw_write_submit_start,
-cdt*:::gw_flush_submit_start
-/arg1 == 1/
-{
-    start1[arg0] = timestamp;
-}
-cdt*:::gw_read_submit_start,
-cdt*:::gw_write_submit_start,
-cdt*:::gw_flush_submit_start
-/arg1 == 2/
-{
-    start2[arg0] = timestamp;
+    start[arg0, arg1] = timestamp;
 }
 
 /*
  * When a read ACK comes back, calculate the delta and store it
  */
 cdt*:::gw_read_submit_end
-/start0[arg0] != 0 && arg1 == 0/
+/start[arg0, arg1] != 0/
 {
-    @time["read downstairs client 0"] = quantize(timestamp - start0[arg0]);
-    start0[arg0] = 0;
-}
-cdt*:::gw_read_submit_end
-/start1[arg0] != 0 && arg1 == 1/
-{
-    @time["read downstairs client 1"] = quantize(timestamp - start1[arg0]);
-    start1[arg0] = 0;
-}
-cdt*:::gw_read_submit_end
-/start2[arg0] != 0 && arg1 == 2/
-{
-    @time["read downstairs client 2"] = quantize(timestamp - start2[arg0]);
-    start2[arg0] = 0;
+    @time["read", arg1] = quantize(timestamp - start[arg0, arg1]);
+    start[arg0, arg1] = 0;
 }
 
 /*
  * When a write ACK comes back, calculate the delta and store it
  */
 cdt*:::gw_write_submit_end
-/start0[arg0] != 0 && arg1 == 0/
+/start[arg0, arg1] != 0/
 {
-    @time["write downstairs client 0"] = quantize(timestamp - start0[arg0]);
-    start0[arg0] = 0;
-}
-cdt*:::gw_write_submit_end
-/start1[arg0] != 0 && arg1 == 1/
-{
-    @time["write downstairs client 1"] = quantize(timestamp - start1[arg0]);
-    start1[arg0] = 0;
-}
-cdt*:::gw_write_submit_end
-/start2[arg0] != 0 && arg1 == 2/
-{
-    @time["write downstairs client 2"] = quantize(timestamp - start2[arg0]);
-    start2[arg0] = 0;
+    @time["write", arg1] = quantize(timestamp - start[arg0, arg1]);
+    start[arg0, arg1] = 0;
 }
 
 /*
  * When a flush ACK comes back, calculate the delta and store it
  */
 cdt*:::gw_flush_submit_end
-/start0[arg0] != 0 && arg1 == 0/
+/start[arg0, arg1] != 0/
 {
-    @time["flush downstairs client 0"] = quantize(timestamp - start0[arg0]);
-    start0[arg0] = 0;
-}
-cdt*:::gw_flush_submit_end
-/start1[arg0] != 0 && arg1 == 1/
-{
-    @time["flush downstairs client 1"] = quantize(timestamp - start1[arg0]);
-    start1[arg0] = 0;
-}
-cdt*:::gw_flush_submit_end
-/start2[arg0] != 0 && arg1 == 2/
-{
-    @time["flush downstairs client 2"] = quantize(timestamp - start2[arg0]);
-    start2[arg0] = 0;
+    @time["flush", arg1] = quantize(timestamp - start[arg0, arg1]);
+    start[arg0, arg1] = 0;
 }
