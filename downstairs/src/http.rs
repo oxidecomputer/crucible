258fb7922da4efa8d3a189881bc864a650c7232f
James MacMahon <james@oxide.computer>
2022-02-17 13:47:01 -0500

    added ServerContext::downstairs comment explaining key uuid

--- downstairs/src/http.rs
+++ downstairs/src/http.rs
@@ -9,6 +9,7 @@ use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 pub struct ServerContext {
+    // Region UUID -> a running Downstairs
     downstairs: Mutex<HashMap<Uuid, Arc<Mutex<Downstairs>>>>,
 }
 
