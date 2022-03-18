195a5544f4cde7dad47304061a954aa9a7e753ed
Joshua M. Clulow <jmc@oxide.computer>
2022-03-18 15:27:16 -0700

    buildomat: first cut at zone image publish job

---
+++ .github/buildomat/jobs/image.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#:
+#: name = "image"
+#: variety = "basic"
+#: target = "helios"
+#: rust_toolchain = "nightly"
+#: output_rules = [
+#:	"/out/*",
+#: ]
+#:
+#: [[publish]]
+#: series = "image"
+#: name = "crucible.tar.gz"
+#: from_output = "/out/crucible.tar.gz"
+#:
+#: [[publish]]
+#: series = "image"
+#: name = "crucible.sha256.txt"
+#: from_output = "/out/crucible.sha256.txt"
+#:
+
+set -o errexit
+set -o pipefail
+set -o xtrace
+
+cargo --version
+rustc --version
+
+banner build
+ptime -m cargo build --release --verbose
+
+banner image
+ptime -m cargo run --bin crucible-package
+
+banner contents
+tar tvfz out/crucible.tar.gz
+
+banner copy
+pfexec mkdir -p /out
+pfexec chown "$UID" /out
+mv out/crucible.tar.gz /out/crucible.tar.gz
+cd /out
+digest -a sha256 crucible.tar.gz > crucible.sha256.txt
