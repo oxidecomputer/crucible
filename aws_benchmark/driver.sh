cf0b91a9bec1d72e85aa86c0f2f81df7d6aa0b41
Alan Hanson <alan@oxidecomputer.com>
2022-04-08 08:02:25 -0700

    PR comments

--- aws_benchmark/driver.sh
+++ aws_benchmark/driver.sh
@@ -25,7 +25,6 @@ program_required virtualenv
 program_required python3
 program_required pip
 program_required aws
-program_required ansible-playbook
 
 # bring up resources, run the benchmark, and clean up
 ./bring_up_resources.sh "${OS}" "${REGION}"
