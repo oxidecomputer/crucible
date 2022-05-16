7270d3cd23bb37ebae52206fab3a7a01369ba1d0
Alan Hanson <alan@oxide.computer>
2022-05-16 19:58:24 +0000

    add needed scripts for repair test

--- tools/test_repair.sh
+++ tools/test_repair.sh
@@ -15,10 +15,13 @@ function ctrl_c() {
 }
 
 function cleanup() {
-    kill "$ds0_pid" 2> /dev/null
-    kill "$ds1_pid" 2> /dev/null
-    kill "$ds2_pid" 2> /dev/null
+    kill "$ds0_pid" 2> /dev/null || true
+    kill "$ds1_pid" 2> /dev/null || true
+    kill "$ds2_pid" 2> /dev/null || true
 }
+
+set -o errexit
+set -o pipefail
 
 SECONDS=0
 ROOT=$(cd "$(dirname "$0")/.." && pwd)
@@ -26,7 +29,7 @@ BINDIR=${BINDIR:-$ROOT/target/debug}
 
 cds="$BINDIR/crucible-downstairs"
 cc="$BINDIR/crucible-client"
-for bin in $cds $cc $ch; do
+for bin in $cds $cc; do
     if [[ ! -f "$bin" ]]; then
         echo "Can't find crucible binary at $bin" >&2
         exit 1
@@ -77,17 +80,17 @@ for (( i = 0; i < 10; i += 1 )); do
     # stop a downstairs and restart with lossy
     if [[ $choice -eq 0 ]]; then
         kill "$ds0_pid"
-        wait "$ds0_pid"
+        wait "$ds0_pid" || true
         ${cds} run -d var/8810 -p 8810 --lossy &> "$ds_log_prefix"8810.txt &
         ds0_pid=$!
     elif [[ $choice -eq 1 ]]; then
         kill "$ds1_pid"
-        wait "$ds1_pid"
+        wait "$ds1_pid" || true
         ${cds} run -d var/8820 -p 8820 --lossy &> "$ds_log_prefix"8820.txt &
         ds1_pid=$!
     else
         kill "$ds2_pid"
-        wait "$ds2_pid"
+        wait "$ds2_pid" || true
         ${cds} run -d var/8830 -p 8830 --lossy &> "$ds_log_prefix"8830.txt &
         ds2_pid=$!
     fi
@@ -103,18 +106,20 @@ for (( i = 0; i < 10; i += 1 )); do
     # Stop --lossy downstairs so it can't complete all its IOs
     if [[ $choice -eq 0 ]]; then
         kill "$ds0_pid"
-        wait "$ds0_pid"
+        wait "$ds0_pid" || true
     elif [[ $choice -eq 1 ]]; then
         kill "$ds1_pid"
-        wait "$ds1_pid"
+        wait "$ds1_pid" || true
     else
         kill "$ds2_pid"
-        wait "$ds2_pid"
+        wait "$ds2_pid" || true
     fi
 
     # Did we get any mismatches?
+    # We || true because dump will return non-zero when it finds
+    # a mismatch
     echo "Current downstairs dump:"
-    ${cds} dump -d var/8810 -d var/8820 -d var/8830
+    ${cds} dump -d var/8810 -d var/8820 -d var/8830 || true
     echo "On loop $i"
 
     echo ""
