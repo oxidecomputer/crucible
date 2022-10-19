f49476a69048f84414779b2907762c03e65b7c75
James MacMahon <james@oxide.computer>
2022-10-19 15:09:20 -0400

    max bytes 512k
    remove AnyhowFromString

--- pantry/src/server.rs
+++ pantry/src/server.rs
@@ -14,16 +14,6 @@ use std::net::SocketAddr;
 use std::result::Result as SResult;
 use std::sync::Arc;
 
-trait AnyhowFromString<T> {
-    fn or_bail(self, msg: &str) -> Result<T>;
-}
-
-impl<T> AnyhowFromString<T> for SResult<T, String> {
-    fn or_bail(self, msg: &str) -> Result<T> {
-        self.map_err(|e| anyhow!("{}: {:?}", msg, e))
-    }
-}
-
 pub fn make_api() -> Result<dropshot::ApiDescription<Arc<Pantry>>> {
     let mut api = dropshot::ApiDescription::new();
 
@@ -40,7 +30,7 @@ pub async fn run_server(
     let server = dropshot::HttpServerStarter::new(
         &dropshot::ConfigDropshot {
             bind_address,
-            request_body_max_bytes: 1024 * 10,
+            request_body_max_bytes: 512 * 1024,
             ..Default::default()
         },
         api,
