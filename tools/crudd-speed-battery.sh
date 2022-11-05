3a92afd447290b88de3a952904cb21e5a58bb6d0
Artemis Everfree <artemis@oxidecomputer.com>
2022-11-05 09:51:02 -0700

    remove traceback debug

--- tools/crudd-speed-battery.sh
+++ tools/crudd-speed-battery.sh
@@ -2,44 +2,6 @@
 
 set -euo pipefail
 
-# == tracebacks from https://docwhat.org/tracebacks-in-bash ==
-trap _exit_trap EXIT
-trap _err_trap ERR
-_showed_traceback=f
-
-function _exit_trap() {
-  local _ec="$?"
-  if [[ $_ec != 0 && ${_showed_traceback} != t ]]; then
-    traceback 1
-  fi
-}
-
-function _err_trap() {
-  local _ec="$?"
-  local _cmd="${BASH_COMMAND:-unknown}"
-  traceback 1
-  _showed_traceback=t
-  echo "The command ${_cmd} exited with exit code ${_ec}." 1>&2
-}
-
-function traceback() {
-  # Hide the traceback() call.
-  local -i start=$((${1:-0} + 1))
-  local -i end=${#BASH_SOURCE[@]}
-  local -i i=0
-  local -i j=0
-
-  echo "Traceback (last called is first):" 1>&2
-  for ((i = start; i < end; i++)); do
-    j=$((i - 1))
-    local function="${FUNCNAME[$i]}"
-    local file="${BASH_SOURCE[$i]}"
-    local line="${BASH_LINENO[$j]}"
-    echo "     ${function}() in ${file}:${line}" 1>&2
-  done
-}
-# == end tracebacks ==
-
 REGION_SIZE_MIBS=4096
 REGION_SIZE_BYTES=$(( REGION_SIZE_MIBS * 1024 * 1024 ))
 BLOCK_SIZE=512
