47269f8f775e8435fe5d09d0e858e2e2a85abde9
Artemis Everfree <artemis@oxidecomputer.com>
2022-11-04 23:55:45 -0700

    Add crudd benchmarks to CI
    This commit adds crudd benchmarks to CI. Three main parts
    
    - Benchmarking mode in crudd
        - no input or output required
            - uses /dev/zero and /dev/null respectively
        - Writes the number of bytes written to a specified file, useful
          because
        - It responds to SIGUSR1 by cleanly exiting early.
            - Could I have done this by implementing the timeout in crudd?
              yes. Did I? no. I'm just using the `timeout` command.
        - This mode is ONLY intended for this usecase and basically serves
          no other useful purpose
    
    - tools/crudd-speed-battery.sh
        - Runs a bunch of tests with different pipeline lengths and request
          sizes
            - Read from unitialized region
            - Write to unintialized region
            - Read from initialized region (zeroes)
            - Write to initialized region (zeroes)
            - Current request sizes: 1/16/256/4096/32768 blocks
            - Current pipeline lengths: 2/4/6/8 requests in flight.
        - Each iteration is limited to 2 minutes maximum runtime
            - The tradeoff here is, when you lower the runtime you get
              faster CI executions, but you also get less data resolution.
            - Could probably go down to 1 minute and still get some useful
              coarse data (orders of magnitude).
            - Can't go too low or constant overhead overshadows the
              meaningful data instead of being a rounding error.
        - 4 (read/write/read/write) * 4 (pipeline lengths) * 5 (request
        - Measured speeds are written to
          /tmp/crudd-speed-battery-results.json
            - Speeds are measured in bytes/second
            - 1 line per (request size, pipeline length) pair
            - Example: `{ "request_size": 32768, "pipeline_length": 8, "read_speed_uninit": 97612893, "write_speed_uninit": 44739242, "read_speed_init": 53024287, "write_speed_init": 28666581 }`
          sizes) * 2 minutes = 160 minutes max runtime for this job as
          currently written.
    - buildomat job
        - runs crudd-speed-battery.sh
        - provides crudd-speed-battery-results.json as output.
    
    Additionally I fixed a crudd bug introduced during the async refactoring
    that I missed during review of that refactoring which caused crudd not
    to actually pipeline its requests, instead issuing them in batches and
    waiting on one batch before starting the next.
    
    Some future work is needed on the graphs repo to add support for
    graphing this data.

---
+++ tools/crudd-speed-battery.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# == tracebacks from https://docwhat.org/tracebacks-in-bash ==
+trap _exit_trap EXIT
+trap _err_trap ERR
+_showed_traceback=f
+
+function _exit_trap() {
+  local _ec="$?"
+  if [[ $_ec != 0 && ${_showed_traceback} != t ]]; then
+    traceback 1
+  fi
+}
+
+function _err_trap() {
+  local _ec="$?"
+  local _cmd="${BASH_COMMAND:-unknown}"
+  traceback 1
+  _showed_traceback=t
+  echo "The command ${_cmd} exited with exit code ${_ec}." 1>&2
+}
+
+function traceback() {
+  # Hide the traceback() call.
+  local -i start=$((${1:-0} + 1))
+  local -i end=${#BASH_SOURCE[@]}
+  local -i i=0
+  local -i j=0
+
+  echo "Traceback (last called is first):" 1>&2
+  for ((i = start; i < end; i++)); do
+    j=$((i - 1))
+    local function="${FUNCNAME[$i]}"
+    local file="${BASH_SOURCE[$i]}"
+    local line="${BASH_LINENO[$j]}"
+    echo "     ${function}() in ${file}:${line}" 1>&2
+  done
+}
+# == end tracebacks ==
+
+REGION_SIZE_MIBS=4096
+REGION_SIZE_BYTES=$(( REGION_SIZE_MIBS * 1024 * 1024 ))
+BLOCK_SIZE=512
+REGION_SIZE_BLOCKS=$(( REGION_SIZE_BYTES / BLOCK_SIZE ))
+# 128 MiB
+EXTENT_SIZE_BLOCKS=$(( 128 * 1024 * 1024 / BLOCK_SIZE ))
+EXTENT_COUNT=$(( REGION_SIZE_BLOCKS / EXTENT_SIZE_BLOCKS ))
+
+MAX_ITERATION_DURATION=$((2 * 60))
+
+BENCHMARK_RESULTS_TMPFILE=/tmp/crudd-benchmark-num-bytes-processed.txt
+
+DOWNSTAIRS_1=127.0.0.1:8810
+DOWNSTAIRS_2=127.0.0.1:8820
+DOWNSTAIRS_3=127.0.0.1:8830
+
+print_err() {
+  printf '%s\n' "$1" 1>&2
+}
+
+# $1 = mode (read or write)
+# $2 = request size in blocks
+# $3 = pipeline length
+# runs for a maximum time specified by the use of timeout in the function
+# prints the average throughput in bytes/second to stdout.
+crudd_benchmark() {
+  local mode="$1"
+  local req_size="$2"
+  local pipeline_len="$3"
+
+  local start_time="$(date '+%s')"
+
+  # SIGUSR1 is handled specially by crudd in bechmark mode
+  # In benchmark mode, crudd prints how many bytes it wrote to
+  # the file passed as an argument to --benchmarking-mode. the timeout
+  # ensures crudd never runs _longer_ than the provided duration, but it could
+  # run shorter if we're fast and the region is small. So we take bytes written,
+  # divided by the time, to get our actual speed. This is all a little bit jank,
+  # admittedly.
+  timeout --signal=USR1 "$MAX_ITERATION_DURATION" \
+    "$BINDIR/crudd" -t $DOWNSTAIRS_1 -t $DOWNSTAIRS_2 -t $DOWNSTAIRS_3 \
+      -n $REGION_SIZE_BYTES -i "$req_size" -p "$pipeline_len" \
+      --benchmarking-mode "$BENCHMARK_RESULTS_TMPFILE" \
+      "$mode" \
+      1>&2
+
+  local end_time="$(date +'%s')"
+  local bytes_processed="$(cat "$BENCHMARK_RESULTS_TMPFILE")"
+
+  # We lose some precision here, but with speeds in the KiB/s or MiB/s, the
+  # rounding loss is not much concern
+  printf '%s' $((bytes_processed / (end_time - start_time) ))
+}
+
+# create and start downstairs with dsc
+create_and_start_downstairs() {
+  if [[ -n ${DSC_PID-} ]]; then
+    print_err "Error: DSC is already started, but someone tried to start it a second time. Current PID is allegedly $DSC_PID"
+    return 1
+  fi
+  "$BINDIR/dsc" start --cleanup \
+    --block-size $BLOCK_SIZE \
+    --create \
+    --extent-size $EXTENT_SIZE_BLOCKS \
+    --extent-count $EXTENT_COUNT \
+  1>&2 &
+  DSC_PID=$!
+  sleep 3
+  if ! pgrep -P $DSC_PID > /dev/null; then
+    print_err "Failed to start dsc"
+    return 1
+  fi
+  print_err "Started DSC $DSC_PID"
+}
+
+# shut down downstairs
+stop_downstairs() {
+  if ps -p $DSC_PID > /dev/null; then
+    print_err "Shutdown $DSC_PID"
+    "$BINDIR/dsc" cmd shutdown 1>&2
+    print_err "Wait on pid $DSC_PID"
+    wait $DSC_PID
+    print_err "$DSC_PID dead"
+  else
+    print_err "tried to stop downstairs, but there is no downstairs"
+    return 1
+  fi
+  DSC_PID=""
+}
+
+
+# - create a region, start up downstairs
+# - test read/write before filling the region with data, and read/write after
+#   it has data
+# - shut down the downstairs
+# - print speed results to stdout tab-separated
+#   - read speed on an uninitialized region
+#   - write speed on an uninitialized region
+#   - read speed on a zero-intitialized region
+#   - write speed on a zero-initialized region
+# $1 = request size
+# $2 = pipeline length
+perform_benchmarks_with_parameters() {
+  # start up downstairs with a fresh region
+  create_and_start_downstairs
+
+  local req_size="$1"
+  local pipeline_len="$2"
+
+  # test read speed before writing any data
+  local read_speed_uninit=$(crudd_benchmark read "$req_size" "$pipeline_len")
+
+  # test write speed before writing any data (first write)
+  local write_speed_uninit=$(crudd_benchmark write "$req_size" "$pipeline_len")
+
+  # make sure the entire region is written
+  # we use a close to optimal write pattern to make this quick regardless of
+  # the benchmark we're running, since we're not timing it out
+  "$BINDIR/crudd" -t $DOWNSTAIRS_1 -t $DOWNSTAIRS_2 -t $DOWNSTAIRS_3 \
+    -n $REGION_SIZE_BYTES -i 32768 -p 4 \
+    write \
+    < /dev/zero 1>&2
+
+
+  # test read speed after writing data
+  local read_speed_init=$(crudd_benchmark read "$req_size" "$pipeline_len")
+
+  # test write speed after writing data
+  local write_speed_init=$(crudd_benchmark write "$req_size" "$pipeline_len")
+
+  # shut it down
+  stop_downstairs
+
+  # print tab separated data as output
+  printf '%s\t%s\t%s\t%s' "$read_speed_uninit" "$write_speed_uninit" "$read_speed_init" "$write_speed_init"
+}
+
+main() {
+  # the maximum amount of time for the full test suite is
+  # number of req_size elems * number of pipeline_len elems * 4 * MAX_ITERATION_DURATION
+  # 5 * 4 * 4 * 2 = 160 minutes max
+  # shellcheck disable=SC1004
+  for req_size in 1 16 256 4096 32768; do
+    for pipeline_len in 2 4 6 8; do
+
+      printf '%s\t%s\t%s\n' \
+        $req_size \
+        $pipeline_len \
+        "$(perform_benchmarks_with_parameters $req_size $pipeline_len)"
+    done
+  done \
+  | awk '
+    {
+      request_size = $1;
+      pipeline_length = $2;
+      read_speed_uninit = $3;
+      write_speed_uninit = $4;
+      read_speed_init = $5;
+      write_speed_init = $6;
+
+      # json time
+      print ( \
+        "{ " \
+        "\"request_size\": "  request_size ", " \
+        "\"pipeline_length\": "  pipeline_length ", " \
+        "\"read_speed_uninit\": "  read_speed_uninit ", " \
+        "\"write_speed_uninit\": "  write_speed_uninit ", " \
+        "\"read_speed_init\": "  read_speed_init ", " \
+        "\"write_speed_init\": "  write_speed_init \
+        " }" \
+      );
+
+    }
+  ' \
+  | tee /tmp/crudd-speed-battery-results.json
+}
+
+main
