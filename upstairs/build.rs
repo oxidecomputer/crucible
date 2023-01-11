084bae044a0db1fcfdf515faac0b678e1132e42c
Benjamin Naecker <ben@oxide.computer>
2023-01-11 20:55:32 +0000

    Better detection of features needed for USDT

--- upstairs/build.rs
+++ upstairs/build.rs
@@ -2,17 +2,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-// Copyright 2022 Oxide Computer Company
+// Copyright 2023 Oxide Computer Company
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    if version_check::is_min_version("1.59").unwrap_or(false) {
-        println!("cargo:rustc-cfg=usdt_stable_asm");
+    if !version_check::is_min_version("1.59").unwrap_or(false) {
+        println!("cargo:rustc-cfg=usdt_need_asm");
     }
 
     #[cfg(target_os = "macos")]
-    if version_check::is_min_version("1.66").unwrap_or(false) {
-        println!("cargo:rustc-cfg=usdt_stable_asm_sym");
+    if version_check::supports_feature("asm_sym").unwrap_or(false)
+        && !version_check::is_min_version("1.67").unwrap_or(false)
+    {
+        println!("cargo:rustc-cfg=usdt_need_asm_sym");
     }
 }
