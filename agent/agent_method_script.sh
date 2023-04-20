bfafac89e520d6aba41706bc276563c0fd6d58a8
Sean Klein <sean@oxide.computer>
2023-04-19 23:59:15 -0400

    Review feedback

--- agent/agent_method_script.sh
+++ agent/agent_method_script.sh
@@ -2,6 +2,9 @@
 
 set -o errexit
 set -o pipefail
+set -o xtrace
+
+. /lib/svc/share/smf_include.sh
 
 DATALINK="$(svcprop -c -p config/datalink "${SMF_FMRI}")"
 GATEWAY="$(svcprop -c -p config/gateway "${SMF_FMRI}")"
@@ -11,6 +14,11 @@ LISTEN_PORT="$(svcprop -c -p config/listen_port "${SMF_FMRI}")"
 PORTBASE="$(svcprop -c -p config/portbase "${SMF_FMRI}")"
 DOWNSTAIRS_PREFIX="$(svcprop -c -p config/downstairs_prefix "${SMF_FMRI}")"
 SNAPSHOT_PREFIX="$(svcprop -c -p config/snapshot_prefix "${SMF_FMRI}")"
+
+if [[ $DATALINK == unknown ]] || [[ $GATEWAY == unknown ]]; then
+    printf 'ERROR: missing datalink or gateway' >&2
+    exit "$SMF_EXIT_ERR_CONFIG"
+fi
 
 ipadm show-addr "$DATALINK/ll" || ipadm create-addr -t -T addrconf "$DATALINK/ll"
 ipadm show-addr "$DATALINK/omicron6"  || ipadm create-addr -t -T static -a "$LISTEN_ADDR" "$DATALINK/omicron6"
--- pantry/pantry_method_script.sh
+++ pantry/pantry_method_script.sh
@@ -2,11 +2,19 @@
 
 set -o errexit
 set -o pipefail
+set -o xtrace
+
+. /lib/svc/share/smf_include.sh
 
 DATALINK="$(svcprop -c -p config/datalink "${SMF_FMRI}")"
 GATEWAY="$(svcprop -c -p config/gateway "${SMF_FMRI}")"
 LISTEN_ADDR="$(svcprop -c -p config/listen_addr "${SMF_FMRI}")"
 LISTEN_PORT="$(svcprop -c -p config/listen_port "${SMF_FMRI}")"
+
+if [[ $DATALINK == unknown ]] || [[ $GATEWAY == unknown ]]; then
+    printf 'ERROR: missing datalink or gateway' >&2
+    exit "$SMF_EXIT_ERR_CONFIG"
+fi
 
 ipadm show-addr "$DATALINK/ll" || ipadm create-addr -t -T addrconf "$DATALINK/ll"
 ipadm show-addr "$DATALINK/omicron6"  || ipadm create-addr -t -T static -a "$LISTEN_ADDR" "$DATALINK/omicron6"
