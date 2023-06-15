7d91e88e44b6c2f6f91e3b1e80807651fef06e8b
Alan Hanson <alan@oxidecomputer.com>
2023-06-15 14:21:59 -0700

    DTrace meet cmon
    Added plumbing in cmon to take input from dtrace output and format things
    all pretty.  This required making some things pub in crucible upstairs so
    cmon could make use of them.  Cmon can now print out just the fields you
    want from dtrace with less effort than making a new dtrace script.
    
    Fixed the dtrace stat recording for reconcile messages, it was incorrectly
    using the live repair stats.
    
    Fixed a log message to correctly report the function it was in.
    
    Removed a debug log message from dsc.

--- cmon/src/main.rs
+++ cmon/src/main.rs
@@ -1,13 +1,16 @@
 // Copyright 2022 Oxide Computer Company
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 use crucible_control_client::Client;
+use std::fmt;
+use std::io::{self, BufRead};
 use tokio::time::{sleep, Duration};
+
+use crucible::Arg;
 
 /// Connect to crucible control server
 #[derive(Parser, Debug)]
 #[clap(name = "cmon", term_width = 80)]
 #[clap(about = "Crucible monitoring tool", long_about = None)]
-
 struct Args {
     #[clap(subcommand)]
     action: Action,
@@ -21,11 +24,50 @@ struct Args {
     seconds: u64,
 }
 
+// The possible fields we will display when receiving DTrace output.
+#[derive(Debug, Copy, Clone, ValueEnum)]
+enum DtraceDisplay {
+    State,
+    IoCount,
+    UpCount,
+    DsCount,
+    Reconcile,
+    LiveRepair,
+    Connected,
+    Replaced,
+    FlowControl,
+    ExtentRepair,
+}
+
+impl fmt::Display for DtraceDisplay {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DtraceDisplay::State => write!(f, "state"),
+            DtraceDisplay::IoCount => write!(f, "io_count"),
+            DtraceDisplay::UpCount => write!(f, "up_count"),
+            DtraceDisplay::DsCount => write!(f, "ds_count"),
+            DtraceDisplay::Reconcile => write!(f, "reconcile"),
+            DtraceDisplay::LiveRepair => write!(f, "live_repair"),
+            DtraceDisplay::Connected => write!(f, "connected"),
+            DtraceDisplay::Replaced => write!(f, "replaced"),
+            DtraceDisplay::FlowControl => write!(f, "flow_control"),
+            DtraceDisplay::ExtentRepair => write!(f, "extent_repair"),
+        }
+    }
+}
+
 #[derive(Debug, Subcommand)]
 enum Action {
+    /// Read from stdin
+    Dtrace {
+        /// Fields to display from dtrace received input
+        #[clap(short, long, default_value = "io-count")]
+        #[arg(value_enum)]
+        output: Vec<DtraceDisplay>,
+    },
     /// Show the current downstairs job queue
     Jobs,
-    /// Show the status of various OnlineRepair stats
+    /// Show the status of various LiveRepair stats
     Repair,
 }
 
@@ -70,7 +112,7 @@ async fn show_work_queue(args: Args) {
     }
 }
 
-// Show OnlineRepair stats
+// Show LiveRepair stats
 async fn show_repair_stats(args: Args) {
     let ca = Client::new(&args.control);
     let mut count = 20;
@@ -110,6 +152,199 @@ async fn show_repair_stats(args: Args) {
         sleep(Duration::from_secs(args.seconds)).await;
     }
 }
+
+// Print out the column headers for the given DtraceDisplay options.
+fn print_dtrace_header(dd: &[DtraceDisplay]) {
+    for display_item in dd.iter() {
+        match display_item {
+            DtraceDisplay::State => {
+                print!(
+                    " {:>16} {:>16} {:>16}",
+                    "DS_0_STATE", "DS_1_STATE", "DS_2_STATE",
+                );
+            }
+            DtraceDisplay::UpCount => {
+                print!(" {:>4}", "UPW");
+            }
+            DtraceDisplay::DsCount => {
+                print!(" {:>4}", "DSW");
+            }
+            DtraceDisplay::IoCount => {
+                print!(" {:4} {:4} {:4}", "NEW0", "NEW1", "NEW2");
+                print!(" {:>4} {:>4} {:>4}", "IP0", "IP1", "IP2");
+                print!(" {:>4} {:>4} {:>4}", "D0", "D1", "D2");
+                print!(" {:>4} {:>4} {:>4}", "S0", "S1", "S2");
+                print!(" {:>4} {:>4} {:>4}", "E0", "E1", "E2");
+            }
+            DtraceDisplay::Reconcile => {
+                print!(" {:>4} {:>4}", "REC", "NEED");
+            }
+            DtraceDisplay::LiveRepair => {
+                print!(" {:>4} {:>4} {:>4}", "LRC0", "LRC1", "LRC0");
+                print!(" {:>4} {:>4} {:>4}", "LRA0", "LRA1", "LRA2");
+            }
+            DtraceDisplay::Connected => {
+                print!(" {:>4} {:>4} {:>4}", "CON0", "CON1", "CON2");
+            }
+            DtraceDisplay::Replaced => {
+                print!(" {:>4} {:>4} {:>4}", "RPL0", "RPL1", "RPL2");
+            }
+            DtraceDisplay::FlowControl => {
+                print!(" {:>4} {:>4} {:>4}", "FC0", "FC1", "FC2");
+            }
+            DtraceDisplay::ExtentRepair => {
+                print!(" {:>4} {:>4} {:>4}", "EXR0", "EXR1", "EXR2");
+                print!(" {:>4} {:>4} {:>4}", "EXC0", "EXC1", "EXC2");
+            }
+        }
+    }
+    println!();
+}
+
+// Print out the values in the dtrace output based on what the DtraceDisplay
+// enums are set in the given Vec.
+fn print_dtrace_row(d_out: Arg, dd: &[DtraceDisplay]) {
+    for display_item in dd.iter() {
+        match display_item {
+            DtraceDisplay::State => {
+                print!(
+                    " {:>16} {:>16} {:>16}",
+                    d_out.ds_state[0].to_string(),
+                    d_out.ds_state[1].to_string(),
+                    d_out.ds_state[2].to_string(),
+                );
+            }
+            DtraceDisplay::UpCount => {
+                print!(" {:4}", d_out.up_count);
+            }
+            DtraceDisplay::DsCount => {
+                print!(" {:4}", d_out.ds_count);
+            }
+            DtraceDisplay::IoCount => {
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_io_count.new[0],
+                    d_out.ds_io_count.new[1],
+                    d_out.ds_io_count.new[2],
+                );
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_io_count.in_progress[0],
+                    d_out.ds_io_count.in_progress[1],
+                    d_out.ds_io_count.in_progress[2],
+                );
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_io_count.done[0],
+                    d_out.ds_io_count.done[1],
+                    d_out.ds_io_count.done[2],
+                );
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_io_count.skipped[0],
+                    d_out.ds_io_count.skipped[1],
+                    d_out.ds_io_count.skipped[2],
+                );
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_io_count.error[0],
+                    d_out.ds_io_count.error[1],
+                    d_out.ds_io_count.error[2],
+                );
+            }
+            DtraceDisplay::Reconcile => {
+                print!(
+                    " {:4} {:4}",
+                    d_out.ds_reconciled, d_out.ds_reconcile_needed
+                );
+            }
+            DtraceDisplay::LiveRepair => {
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_live_repair_completed[0],
+                    d_out.ds_live_repair_completed[1],
+                    d_out.ds_live_repair_completed[2],
+                );
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_live_repair_aborted[0],
+                    d_out.ds_live_repair_aborted[1],
+                    d_out.ds_live_repair_aborted[2],
+                );
+            }
+            DtraceDisplay::Connected => {
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_connected[0],
+                    d_out.ds_connected[1],
+                    d_out.ds_connected[2],
+                );
+            }
+            DtraceDisplay::Replaced => {
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_replaced[0],
+                    d_out.ds_replaced[1],
+                    d_out.ds_replaced[2],
+                );
+            }
+            DtraceDisplay::FlowControl => {
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_flow_control[0],
+                    d_out.ds_flow_control[1],
+                    d_out.ds_flow_control[2],
+                );
+            }
+            DtraceDisplay::ExtentRepair => {
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_extents_repaired[0],
+                    d_out.ds_extents_repaired[1],
+                    d_out.ds_extents_repaired[2],
+                );
+                print!(
+                    " {:4} {:4} {:4}",
+                    d_out.ds_extents_confirmed[0],
+                    d_out.ds_extents_confirmed[1],
+                    d_out.ds_extents_confirmed[2],
+                );
+            }
+        }
+    }
+    println!();
+}
+
+// Take input from stdin (assumed to be output from the dtrace raw script)
+// and print out the fields requested in the output Vec.
+fn dtrace_loop(output: Vec<DtraceDisplay>) {
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+    let mut count = 0;
+    loop {
+        let mut dtrace_out = String::new();
+        match handle.read_line(&mut dtrace_out) {
+            Ok(_) => {
+                if count == 0 {
+                    print_dtrace_header(&output);
+                }
+                count = (count + 1) % 20;
+                let d_out: Arg = match serde_json::from_str(&dtrace_out) {
+                    Ok(a) => a,
+                    Err(e) => {
+                        println!("Err {:?}", e);
+                        continue;
+                    }
+                };
+                print_dtrace_row(d_out, &output);
+            }
+            Err(e) => {
+                println!("Error: {:?}", e);
+            }
+        }
+    }
+}
+
 /*
  * Simple tool to connect to a crucible upstairs control http port
  * and report back the results from a upstairs_fill_info command.
@@ -119,6 +354,9 @@ async fn main() {
     let args = Args::parse();
 
     match args.action {
+        Action::Dtrace { output } => {
+            dtrace_loop(output);
+        }
         Action::Jobs => {
             show_work_queue(args).await;
         }
