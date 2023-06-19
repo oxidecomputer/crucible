2b1e845267ae28b8bcadc4d3e7e55ea7a8ac93fc
Alan Hanson <alan@oxidecomputer.com>
2023-06-19 11:04:33 -0700

    Limit cargo test jobs/threads to 2

--- .github/buildomat/jobs/build-release.sh
+++ .github/buildomat/jobs/build-release.sh
@@ -62,7 +62,7 @@ banner rbuild
 ptime -m cargo build --verbose --release --all-features
 
 banner rtest
-ptime -m cargo test --verbose --jobs 5 -- --test-threads=5
+ptime -m cargo test --verbose --jobs 2 -- --test-threads=2
 
 banner output
 mkdir -p /work/rbins
--- .github/buildomat/jobs/build.sh
+++ .github/buildomat/jobs/build.sh
@@ -48,4 +48,4 @@ echo in_work_bins
 ls -l /work/bins
 
 banner test
-ptime -m cargo test --verbose --jobs 5 -- --test-threads=5
+ptime -m cargo test --verbose --jobs 2 -- --test-threads=2
