70c06ffec9e145706b96c9fcc3418ac5e1079b23
Alan Hanson <alan@oxidecomputer.com>
2023-06-20 13:11:43 -0700

    Increase flush_timeout for some tests, collect cores (#810)
    Increase flush_timeout for some tests, collect cores
    
    Updated the flush_timeout to be 24 hours for tests that don't
    want to get a flush ever.
    
    Added core collection to the build jobs to catch any cores from
    cargo test (or other tests) that fail.

--- .github/buildomat/jobs/build.sh
+++ .github/buildomat/jobs/build.sh
@@ -7,6 +7,8 @@
 #: output_rules = [
 #:	"/work/bins/*",
 #:	"/work/scripts/*",
+#:	"/tmp/core.*",
+#:	"/tmp/*.log",
 #: ]
 #:
 
@@ -16,6 +18,14 @@ set -o xtrace
 
 cargo --version
 rustc --version
+
+banner cores
+pfexec coreadm -i /tmp/core.%f.%p
+pfexec coreadm -g /tmp/core.%f.%p
+pfexec coreadm -e global
+pfexec coreadm -e log
+pfexec coreadm -e proc-setid
+pfexec coreadm -e global-setid
 
 banner build
 ptime -m cargo build --verbose
