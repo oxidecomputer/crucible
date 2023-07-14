7111f9ab57893ef44ec3ebc6456950a49f427fbc
Alan Hanson <alan@oxidecomputer.com>
2023-07-13 23:57:23 -0700

    Minor clippy cleanup (#832)
    Minor clippy cleanup
    add --nocapture to cargo test
    Send cargo test output to a file

--- .github/buildomat/jobs/build-release.sh
+++ .github/buildomat/jobs/build-release.sh
@@ -62,7 +62,7 @@ banner rbuild
 ptime -m cargo build --verbose --release --all-features
 
 banner rtest
-ptime -m cargo test --verbose
+ptime -m cargo test --verbose -- --nocapture > /tmp/cargo-test-out.log 2>&1
 
 banner output
 mkdir -p /work/rbins
