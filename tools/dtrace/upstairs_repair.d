e2f939c818722c56169cf35fede21dd040c5f3b5
Alan Hanson <alan@oxide.computer>
2023-08-21 16:10:35 +0000

    DTrace updates and new live repair
    Fixed two DTrace scripts that needed updates.
    Moved the new live repair test into place.

--- tools/dtrace/upstairs_repair.d
+++ tools/dtrace/upstairs_repair.d
@@ -2,6 +2,7 @@
  * Display internal Upstairs live repair status.
  */
 #pragma D option quiet
+#pragma D option strsize=1k
 /*
  * Print the header right away
  */
@@ -18,8 +19,15 @@ tick-1s
 /show > 20/
 {
     printf("%17s %17s %17s", "DS STATE 0", "DS STATE 1", "DS STATE 2");
-    printf("  %5s %5s %5s %5s %5s %5s",
-        "CONF0", "CONF1", "CONF2", "RPAR0", "RPAR1", "RPAR2");
+    printf(" ");
+
+    /* Header width, three downstairs, space between: 5+1+5+1+5 = 17 */
+    printf(" %17s %17s",
+        "LR_COMPLETED", "LR_ABORTED");
+    printf(" %17s %17s",
+        "CONNECTED", "REPLACED");
+    printf(" %17s %17s",
+        "EXTENTS_REPAIRED", "EXTENTS_CONFIRMED");
     printf("\n");
     show = 0;
 }
@@ -34,16 +42,30 @@ crucible_upstairs*:::up-status
     printf(" %17s", json(copyinstr(arg1), "ok.ds_state[1]"));
     printf(" %17s", json(copyinstr(arg1), "ok.ds_state[2]"));
 
-    /*
-     * Repair counts for the downstairs
-     */
     printf(" ");
-    printf(" %5s", json(copyinstr(arg1), "ok.ds_confirm[0]"));
-    printf(" %5s", json(copyinstr(arg1), "ok.ds_confirm[1]"));
-    printf(" %5s", json(copyinstr(arg1), "ok.ds_confirm[2]"));
-    printf(" %5s", json(copyinstr(arg1), "ok.ds_repair[0]"));
-    printf(" %5s", json(copyinstr(arg1), "ok.ds_repair[1]"));
-    printf(" %5s", json(copyinstr(arg1), "ok.ds_repair[2]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_live_repair_completed[0]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_live_repair_completed[1]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_live_repair_completed[2]"));
+
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_live_repair_aborted[0]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_live_repair_aborted[1]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_live_repair_aborted[2]"));
+
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_connected[0]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_connected[1]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_connected[2]"));
+
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_replaced[0]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_replaced[1]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_replaced[2]"));
+
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_extents_repaired[0]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_extents_repaired[1]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_extents_repaired[2]"));
+
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_extents_confirmed[0]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_extents_confirmed[1]"));
+    printf(" %5s", json(copyinstr(arg1), "ok.ds_extents_confirmed[2]"));
 
     printf("\n");
 }
--- tools/test_live_repair.sh
+++ tools/test_live_repair.sh
@@ -1,75 +1,84 @@
 #!/bin/bash
 
-# Create and start up the downstairs.
-# In a loop:
-#  Send IO through crutest
-#  Fault a downstairs
-#  Let the upstairs repair.
-
+# Create regions and start up four downstairs.
+# We create four and use the forth downstairs as the replacement
+# downstairs.
+# Run the crutest replace (live_repair) test using the downstairs
+# we just started.  Each test lap will do 50 replacements and we
+# verify our volume on start and record what we wrote on exit
+# so the next loop will start assuming to read the data the previous
+# loop wrote.
 err=0
 total=0
 pass_total=0
 SECONDS=0
-dropshot=0
-ulimit -n 65536
 
 # Control-C to cleanup.
 trap ctrl_c INT
 function ctrl_c() {
     echo "Stopping at your request"
-    if ps -p "$dsc_pid" ; then
-        ${dsc} cmd shutdown
-        wait "$dsc_pid"
-    fi
-    if ps -p "$crutest_pid" ; then
-        kill "$crutest_pid"
-        wait "$crutest_pid"
-    fi
+    ${dsc} cmd shutdown
 }
 
 loop_log=/tmp/test_live_repair_summary.log
 test_log=/tmp/test_live_repair.log
-dsc_test_log=/tmp/test_live_repair_dsc.log
-verify_file=/tmp/test_live_verify
 
 ROOT=$(cd "$(dirname "$0")/.." && pwd)
-export BINDIR=${BINDIR:-$ROOT/target/release}
-cds="$BINDIR/crucible-downstairs"
+export BINDIR=${BINDIR:-$ROOT/target/debug}
 crucible_test="$BINDIR/crutest"
 dsc="$BINDIR/dsc"
-for bin in $cds $crucible_test $dsc; do
-    if [[ ! -f "$bin" ]]; then
-        echo "Can't find crucible binary at $bin" >&2
-        exit 1
-    fi
-done
-
-# Verify there is not a downstairs already running.
-if pgrep -fl -U "$(id -u)" "$cds"; then
-    echo "Downstairs already running" >&2
-    echo Run: pkill -f -U "$(id -u)" "$cds" >&2
+downstairs="$BINDIR/crucible-downstairs"
+if [[ ! -f "$crucible_test" ]] || [[ ! -f "$dsc" ]] || [[ ! -f "$downstairs" ]]; then
+    echo "Can't find required binaries"
+    echo "Missing $crucible_test or $dsc or $downstairs"
     exit 1
 fi
+
+loops=5
+
+usage () {
+    echo "Usage: $0 [-l #]]" >&2
+    echo " -l loops   Number of test loops to perform (default 10)" >&2
+}
+
+while getopts 'l:' opt; do
+    case "$opt" in
+        l)  loops=$OPTARG
+            ;;
+        *)  echo "Invalid option"
+            usage
+            exit 1
+            ;;
+    esac
+done
 
 echo "" > ${loop_log}
 echo "" > ${test_log}
-echo "" > ${dsc_test_log}
-if [[ -f "$verify_file" ]]; then
-    rm  ${verify_file}
-fi
 echo "starting $(date)" | tee ${loop_log}
 echo "Tail $test_log for test output"
 
-# Large extents, but not many of them means we are likely to try to write
-# to an extent that is being repaired.
+region_dir1="/stock-0/dsc"
+region_dir2="/stock-1/dsc"
+region_dir3="/stock-2/dsc"
+region_dir4="/stock-3/dsc"
+
 if ! ${dsc} create --cleanup \
-  --ds-bin "$cds" \
-  --extent-count 100 \
-  --extent-size 300 >> "$dsc_test_log"; then
+  --region-count 4 \
+  --ds-bin "$downstairs" \
+  --region-dir ${region_dir1} \
+  --region-dir ${region_dir2} \
+  --region-dir ${region_dir3} \
+  --region-dir ${region_dir4} \
+  --extent-count 50 >> "$test_log"; then
     echo "Failed to create downstairs regions"
     exit 1
 fi
-${dsc} start --ds-bin "$cds" >> "$dsc_test_log" 2>&1 &
+${dsc} start --ds-bin "$downstairs" \
+  --region-count 4 \
+  --region-dir ${region_dir1} \
+  --region-dir ${region_dir2} \
+  --region-dir ${region_dir3} \
+  --region-dir ${region_dir4} >> "$test_log" 2>&1 &
 dsc_pid=$!
 sleep 5
 if ! ps -p $dsc_pid > /dev/null; then
@@ -78,108 +87,64 @@ if ! ps -p $dsc_pid > /dev/null; then
 fi
 
 args=()
-port_base=8810
-for (( i = 0; i < 30; i += 10 )); do
-    (( port = port_base + i ))
-    args+=( -t "127.0.0.1:$port" )
-done
+args+=( -t "127.0.0.1:8810" )
+args+=( -t "127.0.0.1:8820" )
+args+=( -t "127.0.0.1:8830" )
 
 gen=1
 # Initial seed for verify file
 if ! "$crucible_test" fill "${args[@]}" -q -g "$gen"\
-          --verify-out "$verify_file" \
-          --retry-activate >> "$test_log" 2>&1 ; then
+          --verify-out alan --retry-activate >> "$test_log" 2>&1 ; then
     echo Failed on initial verify seed, check "$test_log"
     ${dsc} cmd shutdown
 fi
 (( gen += 1 ))
 
-for i in {1..20}
-do
+# Now run the crutest replace test in a loop
+count=1
+while [[ $count -le $loops ]]; do
     SECONDS=0
-    choice=$((RANDOM % 3))
     echo "" > "$test_log"
-    echo "New loop starts now $(date) faulting: $choice" >> "$test_log"
-    # This has to be long enough that faulting a downstairs will be
-    # noticed, but not so long that the test takes forever.
-    "$crucible_test" generic "${args[@]}" --continuous \
-        -q -g "$gen" --verify-out "$verify_file" \
-        --verify-in "$verify_file" \
-        --control 127.0.0.1:7777 \
-        --retry-activate >> "$test_log" 2>&1 &
-    crutest_pid=$!
-    sleep 5
-
-    # Fault a downstairs.
-    curl -s -X POST http://127.0.0.1:7777/downstairs/fault/"${choice}"
-
-    # Give the fault time to be noticed
-    sleep 5
-
-	# Now wait for all downstairs to be active
-    all_state=$(curl -s http://127.0.0.1:7777/info | awk -F\" '{print $8","$10","$12}')
-    while [[ "${all_state}" != "active,active,active" ]]; do
-        sleep 5
-        all_state=$(curl -s http://127.0.0.1:7777/info | awk -F\" '{print $8","$10","$12}')
-    done
-
-	# All downstairs now active, stop crutest.
-    kill -SIGUSR1 $crutest_pid
-    wait ${crutest_pid}
+    echo "New loop starts now $(date)" >> "$test_log"
+    "$crucible_test" replace "${args[@]}" -c 50 \
+            --replacement 127.0.0.1:8840 \
+            --stable -g "$gen" --verify-out alan \
+            --verify-in alan >> "$test_log" 2>&1
     result=$?
     if [[ $result -ne 0 ]]; then
-        if tail "$test_log" | grep dropshot > /dev/null ; then
-            (( dropshot += 1 ))
-        else
-            (( err += 1 ))
-            duration=$SECONDS
-            printf "[%03d] Error $result after %d:%02d\n" "$i" \
-                    $((duration / 60)) $((duration % 60)) | tee -a ${loop_log}
-            mv "$test_log" "$test_log".lastfail
-            break
-        fi
+        touch /var/tmp/ds_test/up 2> /dev/null
+        (( err += 1 ))
+        duration=$SECONDS
+        printf "[%03d] Error $result after %d:%02d\n" "$count" \
+                $((duration / 60)) $((duration % 60)) | tee -a ${loop_log}
+        mv "$test_log" "$test_log".lastfail
+        break
     fi
-
-    (( gen += 1 ))
-    # Run a verify now
-    if ! "$crucible_test" verify "${args[@]}" -q -g "$gen" \
-      --verify-in "$verify_file" \
-      --control 127.0.0.1:7777 >> "$test_log" 2>&1 ; then
-        if tail "$test_log" | grep dropshot > /dev/null ; then
-            (( dropshot += 1 ))
-        else
-            mv "$test_log" "$test_log".lastfail
-            echo "verify failed on loop $i"
-            (( err += 1 ))
-            break
-        fi
-    fi
-
     duration=$SECONDS
     (( gen += 1 ))
     (( pass_total += 1 ))
     (( total += duration ))
     ave=$(( total / pass_total ))
-    printf \
-      "[%03d][%d] %d:%02d  ds_err:%d ave:%d:%02d total:%d:%02d last_run:%d\n" \
-      "$i" "$choice" \
-      $((duration / 60)) $((duration % 60)) \
-      "$dropshot"  \
-      $((ave / 60)) $((ave % 60))  $((total / 60)) $((total % 60)) \
-      "$duration" | tee -a ${loop_log}
+    printf "[%03d/%03d] %d:%02d  ave:%d:%02d  total:%d:%02d errors:%d \
+last_run_seconds:%d\n" \
+  "$count" "$loops" \
+  $((duration / 60)) $((duration % 60)) \
+  $((ave / 60)) $((ave % 60)) \
+  $((total / 60)) $((total % 60)) \
+  "$err" $duration | tee -a ${loop_log}
+    (( count += 1 ))
 
 done
-
-# Stop dsc.
 ${dsc} cmd shutdown
-wait ${dsc_pid}
+wait "$dsc_pid"
 
+sleep 4
 echo "Final results:" | tee -a ${loop_log}
-printf \
-  "[%03d] %d:%02d  ave:%d:%02d  total:%d:%02d errors:%d last_run_seconds:%d\n" \
-  "$i" $((duration / 60)) $((duration % 60)) \
-  $((ave / 60)) $((ave % 60)) $((total / 60)) $((total % 60)) \
+printf "[%03d] %d:%02d  ave:%d:%02d  total:%d:%02d errors:%d last_run_seconds:%d\n" \
+  "$count" \
+  $((duration / 60)) $((duration % 60)) \
+  $((ave / 60)) $((ave % 60)) \
+  $((total / 60)) $((total % 60)) \
   "$err" $duration | tee -a ${loop_log}
-echo "$(date) Test ends with $err" >> "$test_log"
+echo "$(date) Test ends with $err" >> "$test_log" 2>&1
 exit "$err"
-
