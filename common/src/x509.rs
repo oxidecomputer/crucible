4dfa133669e81557ac2d63cae9eee1091848f47d
Patrick Mooney <pmooney@pfmooney.com>
2023-08-25 12:03:09 -0500

    Update tokio-rustls to 0.24.1

--- common/src/x509.rs
+++ common/src/x509.rs
@@ -1,6 +1,7 @@
 // Copyright 2022 Oxide Computer Company
 use std::fs::File;
 use std::io::{self, BufReader};
+use std::sync::Arc;
 
 // Reference tokio-rustls repo examples/server/src/main.rs
 use rustls_pemfile::{certs, rsa_private_keys};
@@ -25,9 +26,6 @@ pub fn load_rsa_keys(path: &str) -> io::Result<Vec<PrivateKey>> {
 
 #[derive(thiserror::Error, Debug)]
 pub enum TLSContextError {
-    #[error("PKI error")]
-    PKIError(#[from] tokio_rustls::webpki::Error),
-
     #[error("IO error")]
     IOError(#[from] std::io::Error),
 
@@ -64,7 +62,7 @@ impl TLSContext {
         Ok(ClientConfig::builder()
             .with_safe_defaults()
             .with_root_certificates(self.root_cert_store.clone())
-            .with_single_cert(self.certs.clone(), self.keys[0].clone())?)
+            .with_client_auth_cert(self.certs.clone(), self.keys[0].clone())?)
     }
 
     pub fn get_server_config(&self) -> Result<ServerConfig, TLSContextError> {
@@ -73,7 +71,7 @@ impl TLSContext {
 
         Ok(ServerConfig::builder()
             .with_safe_defaults()
-            .with_client_cert_verifier(client_cert_verifier)
+            .with_client_cert_verifier(Arc::new(client_cert_verifier))
             .with_single_cert(self.certs.clone(), self.keys[0].clone())?)
     }
 }
