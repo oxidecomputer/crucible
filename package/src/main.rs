0d450bb2b2d9c0f301047f09035b7a712caca2b5
Sean Klein <sean@oxide.computer>
2024-01-15 14:46:28 -0800

    Use the omicron-zone-package methods for topo sorting

--- package/src/main.rs
+++ package/src/main.rs
@@ -1,13 +1,10 @@
 // Copyright 2022 Oxide Computer Company
 
-use anyhow::{bail, Result};
+use anyhow::Result;
 use omicron_zone_package::config;
-use omicron_zone_package::package::{PackageOutput, PackageSource};
 use omicron_zone_package::target::Target;
-use std::collections::BTreeMap;
 use std::fs::create_dir_all;
 use std::path::Path;
-use topological_sort::TopologicalSort;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -16,59 +13,12 @@ async fn main() -> Result<()> {
     let output_dir = Path::new("out");
     create_dir_all(output_dir)?;
 
-    // Reverse lookup of "output" -> "package creating this output".
-    let all_packages = cfg
-        .packages
-        .iter()
-        .map(|(name, package)| (package.get_output_file(name), (name, package)))
-        .collect::<BTreeMap<_, _>>();
+    let packages = cfg.packages_to_deploy(&Target::default());
+    let mut package_iter = packages.build_order();
 
-    // Collect all packages, and sort them in dependency order,
-    // so we know which ones to build first.
-    let mut outputs = TopologicalSort::<String>::new();
-    for (package_output, (_, package)) in &all_packages {
-        match &package.source {
-            PackageSource::Local { .. }
-            | PackageSource::Prebuilt { .. }
-            | PackageSource::Manual => {
-                // Skip intermediate leaf packages; if necessary they'll be
-                // added to the dependency graph by whatever composite package
-                // actually depends on them.
-                if !matches!(
-                    package.output,
-                    PackageOutput::Zone {
-                        intermediate_only: true
-                    }
-                ) {
-                    outputs.insert(package_output);
-                }
-            }
-            PackageSource::Composite { packages: deps } => {
-                for dep in deps {
-                    outputs.add_dependency(dep, package_output);
-                }
-            }
-        }
-    }
-
-    while !outputs.is_empty() {
-        let batch = outputs.pop_all();
-        assert!(
-            !batch.is_empty() || outputs.is_empty(),
-            "cyclic dependency in package manifest!"
-        );
-
-        for output in &batch {
-            println!("Creating '{output}'");
-
-            let Some((name, package)) = all_packages.get(output) else {
-                bail!(
-                    "Cannot find a package to create output: '{output}' \n\
-                     \tThis can happen when building a composite package, where one of \n\
-                     \tthe 'source.packages' has not been found."
-                );
-            };
-
+    while let Some(batch) = package_iter.next() {
+        for (name, package) in &batch {
+            println!("Building '{name}'");
             package
                 .create_for_target(&Target::default(), &name, output_dir)
                 .await?;
