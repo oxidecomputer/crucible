aa647d88d51bfc3d23b81138c58e2c32ce29c479
Alan Hanson <alan@oxide.computer>
2024-04-03 22:18:39 +0000

    Use -p

--- tools/dtrace/get-ds-state.sh
+++ tools/dtrace/get-ds-state.sh
@@ -5,5 +5,5 @@
 for zzz in $(zoneadm list | grep propolis); do
     echo -n "$zzz "
     ppid=$(zlogin "$zzz" pgrep propolis-server)
-    dtrace -xstrsize=1k -q -n 'crucible_upstairs*:::up-status /pid==$1/ { printf("%6d %17s %17s %17s", pid, json(copyinstr(arg1), "ok.ds_state[0]"), json(copyinstr(arg1), "ok.ds_state[1]"), json(copyinstr(arg1), "ok.ds_state[2]")); exit(0); }' $ppid
+    dtrace -xstrsize=1k -p $ppid -q -n 'crucible_upstairs*:::up-status { printf("%6d %17s %17s %17s", pid, json(copyinstr(arg1), "ok.ds_state[0]"), json(copyinstr(arg1), "ok.ds_state[1]"), json(copyinstr(arg1), "ok.ds_state[2]")); exit(0); }'
 done
--- tools/dtrace/get-lr-state.sh
+++ tools/dtrace/get-lr-state.sh
@@ -5,5 +5,5 @@
 for zzz in $(zoneadm list | grep propolis); do
     echo -n "$zzz "
     ppid=$(zlogin "$zzz" pgrep propolis-server)
-    dtrace -xstrsize=1k -q -n 'crucible_upstairs*:::up-status /pid==$1/ { printf("%6d %s %s %s %s %s %s", pid, json(copyinstr(arg1), "ok.ds_live_repair_completed[0]"), json(copyinstr(arg1), "ok.ds_live_repair_completed[1]"), json(copyinstr(arg1), "ok.ds_live_repair_completed[2]"), json(copyinstr(arg1), "ok.ds_live_repair_aborted[0]"), json(copyinstr(arg1), "ok.ds_live_repair_aborted[1]"), json(copyinstr(arg1), "ok.ds_live_repair_aborted[2]")); exit(0); }' $ppid
+    dtrace -xstrsize=1k -p $ppid -q -n 'crucible_upstairs*:::up-status { printf("%6d %s %s %s %s %s %s", pid, json(copyinstr(arg1), "ok.ds_live_repair_completed[0]"), json(copyinstr(arg1), "ok.ds_live_repair_completed[1]"), json(copyinstr(arg1), "ok.ds_live_repair_completed[2]"), json(copyinstr(arg1), "ok.ds_live_repair_aborted[0]"), json(copyinstr(arg1), "ok.ds_live_repair_aborted[1]"), json(copyinstr(arg1), "ok.ds_live_repair_aborted[2]")); exit(0); }'
 done
