e24c9267d649d1ca1946f777a3367a758d480098
Alan Hanson <alan@oxide.computer>
2024-06-25 00:23:43 +0000

    remove paths from dtrace tarball

--- tools/make-dtrace.sh
+++ tools/make-dtrace.sh
@@ -10,10 +10,12 @@ echo "git log -1:" >> dtrace-info.txt
 git log -1 >> dtrace-info.txt
 echo "git status:" >> dtrace-info.txt
 git status >> dtrace-info.txt
+mv dtrace-info.txt tools/dtrace
 
-tar cvf out/crucible-dtrace.tar \
-  tools/dtrace/* \
-  dtrace-info.txt
+pushd tools/dtrace
+tar cvf ../../out/crucible-dtrace.tar \
+  *
 
-ls -l out/crucible-dtrace.tar
 rm dtrace-info.txt
+popd
+ls -l out/crucible-dtrace.tar
