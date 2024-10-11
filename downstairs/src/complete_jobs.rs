158e3fdaf005499edaeb0ef5319b96ae5dbe34a9
Matt Keeter <matt@oxide.computer>
2024-10-11 10:06:42 -0400

    Simplify `CompleteJobs` (#1493)
    We never actually use the "jobs before `last_flush` count as completed"
    property of `CompleteJobs`.
    
    From discussions in chat, this may have been a legacy from when we sent
    literally every previous job in the dependency list.
    Now that flushes act as a full dependency barrier, it shouldn't be
    possible to depend on anything before a flush.
    
    This PR removes special-casing of `last_flush`; instead it becomes first
    among equals in `completed: Vec<JobId>`. Most of the changes are to edit
    test cases that violate the "flushes are a full barrier" property.

--- downstairs/src/complete_jobs.rs
+++ downstairs/src/complete_jobs.rs
@@ -1,24 +1,19 @@
 use crate::JobId;
 
-/// Stores a flush operation and a set of complete jobs
-///
-/// The flush operation is not included in the list of complete jobs, but acts
-/// as a lower bound for dependencies; any job older than the flush is assumed
-/// to have completed.
+/// Stores a set of complete jobs
 #[derive(Debug)]
 pub struct CompletedJobs {
-    last_flush: Option<JobId>,
     completed: Vec<JobId>,
 }
 
 impl CompletedJobs {
     pub fn new(last_flush: Option<JobId>) -> Self {
         Self {
-            last_flush,
-            completed: vec![],
+            completed: last_flush.into_iter().collect(),
         }
     }
 
+    #[cfg(test)]
     pub fn is_empty(&self) -> bool {
         self.completed.is_empty()
     }
@@ -28,28 +23,26 @@ impl CompletedJobs {
         self.completed.push(id);
     }
 
-    /// Resets the data structure given a new barrier operation
+    /// Resets the data structure, given a new barrier operation
+    ///
+    /// All older jobs are forgotten, and the provided operation becomes the
+    /// oldest complete job.
     pub fn reset(&mut self, id: JobId) {
-        self.last_flush = Some(id);
         self.completed.clear();
+        self.completed.push(id);
     }
 
     /// Checks whether the given job is complete
     ///
-    /// A job is complete if it precedes our last barrier operation or is listed
-    /// in the set of complete jobs.
+    /// A job is complete if it is listed in the set of complete jobs.
     pub fn is_complete(&self, id: JobId) -> bool {
         // We deliberately reverse the `completed` list because new jobs are at
         // the back and are more likely to be what we care about
-        self.last_flush.map(|b| id <= b).unwrap_or(false)
-            || self.completed.iter().rev().any(|j| *j == id)
+        self.completed.iter().rev().any(|j| *j == id)
     }
 
+    /// Returns the list of completed jobs
     pub fn completed(&self) -> &[JobId] {
         &self.completed
-    }
-
-    pub fn last_flush(&self) -> Option<JobId> {
-        self.last_flush
     }
 }
