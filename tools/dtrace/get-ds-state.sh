3865ee150d0d7e784868fc4e62d278c47beeb9f1
Alan Hanson <alan@oxidecomputer.com>
2025-01-06 08:49:32 -0800

    DTrace updates (#1589)
    Move dtrace scripts to /opt/oxide/dtrace/crucible, changed places that
    needed to know about the new paths.
    
    Updated many scripts to use three letter short names for downstairs
    state. Updated lookup table to new inline shortcut to reduce code
    duplication. Added WQ and RPL where it was missing. At some point I'll
    add them all, but waiting for Matt's PR to land where we update how the
    states work and I may decide to print things differently.
    ```
    inline string short_state[string ss] =
        ss == "active" ? "ACT" :
        ss == "new" ? "NEW" :
        ss == "replaced" ? "RPL" :
        ss == "live_repair_ready" ? "LRR" :
        ss == "live_repair" ? "LR" :                                          
        ss == "faulted" ? "FLT" :
        ss == "offline" ? "OFL" :                                             
        ss == "reconcile" ? "REC" :
        ss == "wait_quorum" ? "WQ" :
        ss;
    ```
    Fixed column width for PIDs in a number of scripts.
    
    Added example `simple.d` dtrace script from 2024 DTrace conference.
    
    Example output:
    get-up-state.sh:
    ```
    BRM42220030 # /opt/oxide/crucible_dtrace/get-up-state.sh
      PID  SESSION DS0 DS1 DS2   NEXT_JOB  DELTA CONN   ELR   ELC   ERR   ERN
        8 00c767d0 NEW NEW NEW       1000      0    0     0     0     0     0
        8 0c5af3b7 NEW NEW NEW       1000      0    0     0     0     0     0
        8 0cd7bba2 ACT ACT ACT       1000      0    3     0     0     0     0
        8 0dd09245 NEW NEW NEW       1000      0    0     0     0     0     0
        8 121dc7ea ACT ACT ACT       1000      0    3     0     0     0     0
    17774 03419c44 ACT ACT ACT      46479      0    4    74   294     0     0
    17774 f0745404 NEW NEW NEW      32271      0    3     0     0     0     0
    18563 0fd262e1 NEW NEW NEW      32285      0    3     0     0     0     0
    18563 eb102b5a ACT ACT ACT      46210      0    4    74   294     0     0
    19622 4b0ace49 NEW NEW NEW      32271      0    3     0     0     0     0
    19622 d994fe0a ACT ACT ACT      46240      0    4    74   294     0     0
    ```
    
    get-ds-state.sh
    ```
    BRM42220030 # /opt/oxide/crucible_dtrace/get-ds-state.sh 
    oxz_crucible_pantry_8e3b64eb     8 00c767d0  WQ NEW  WQ
    oxz_crucible_pantry_8e3b64eb     8 00c767d0 NEW NEW NEW
    oxz_crucible_pantry_8e3b64eb     8 0c5af3b7 NEW NEW NEW
    oxz_crucible_pantry_8e3b64eb     8 0cd7bba2 ACT ACT ACT
    oxz_propolis-server_d3bdb604 17774 03419c44 ACT ACT ACT
    oxz_propolis-server_d3bdb604 17774 f0745404 NEW NEW NEW
    oxz_propolis-server_bfba345a 18563 eb102b5a ACT ACT ACT
    oxz_propolis-server_bfba345a 18563 eb102b5a NEW NEW NEW
    oxz_propolis-server_335eb83d 19622 4b0ace49 NEW NEW NEW
    oxz_propolis-server_335eb83d 19622 d994fe0a ACT ACT ACT
    ```
    
    Co-authored-by: Alan Hanson <alan@oxide.computer>

--- tools/dtrace/get-ds-state.sh
+++ tools/dtrace/get-ds-state.sh
@@ -4,13 +4,18 @@
 # it finds running on a system.
 filename='/tmp/get-ds-state.out'
 
+# Clear out any previous state
+echo "" > "$filename"
 # Gather state on all running propolis servers, record summary to a file
-dtrace -s /opt/oxide/crucible_dtrace/get-ds-state.d | sort -n | uniq | awk 'NF' > "$filename"
+dtrace -s /opt/oxide/dtrace/crucible/get-ds-state.d | sort -n | uniq | awk 'NF' > "$filename"
 # Walk the lines in the file, append the zone name to each line.
 while read -r p; do
         # For each line in the file, pull out the PID we are looking at and
-        # print the zone that process is running in.
+        # use it to find the zone so we can print the zone name as well.
         pid=$(echo $p | awk '{print $1}')
         zone=$(ps -o zone -p $pid | tail -1 | cut -c 1-28)
-        echo "$zone $p"
+        # Our zone string size is already set from above, force the
+        # rest of the line to take up 26 columns, this prevents PIDs
+        # with fewer than 5 digits from using less columns.
+        printf "%s %26s\n" "$zone" "$p"
 done < "$filename"
