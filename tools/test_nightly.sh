9864b38e08e081fa3d8d4236522b543b9bcf07a8
Alan Hanson <alan@oxidecomputer.com>
2025-04-03 09:43:25 -0700

    No color for git output for nightly test

--- tools/test_nightly.sh
+++ tools/test_nightly.sh
@@ -15,7 +15,8 @@ cd "$ROOT" || (echo failed to cd "$ROOT"; exit 1)
 export BINDIR=${BINDIR:-$ROOT/target/release}
 
 echo "Nightly starts at $(date)" | tee "$output_file"
-echo "Running on $(git log -1 | head -20)" | tee -a "$output_file"
+echo "Running on $(git log -1 --no-color | head -20)" | tee -a "$output_file"
+echo "" >> "$output_file"
 echo "$(date) hammer start" >> "$output_file"
 banner hammer
 banner loop
