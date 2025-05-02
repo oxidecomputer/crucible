853a05e9c68ab79def2c84022aeab2f3575e2375
iximeow <iximeow@oxide.computer>
2025-05-02 21:10:37 +0000

    [agent-client] re-export retry type to untangle Progenitor versioning
    this is the first of two steps in not having to make changes like
    https://github.com/oxidecomputer/omicron/pull/7333, and i'm about to
    make in service of Progenitor 0.10.0.
    
    Omicron needs to have the same Progenitor version as Crucible (and
    interestingly not other dependencies) because Nexus uses a
    `ProgenitorOperationRetry` type to ... retry some kinds of Progenitor
    errors. critically, this same retry type is used for Omicron-internal
    clients with Omicron's Progenitor *and* to wrap clients that Omicron
    uses from external dependencies like `crucible-agent-client`. then, if
    Crucible's Progenitor version is different, Omicron fails to build
    because the retry wrapper uses Omicron-version Progentior when it is
    given a Crucible-version Progenitor instead and the types are not
    identical.
    
    instead, if we depend on omicron-common in crucible-agent-client, export
    *our* version of ProgenitorOperationRetry defined in terms of *our*
    version of Progenitor, users of crucible-agent-client that want to retry
    can use our type instead.
    
    if someone wants to abstract over all clients' ProgenitorOperationRetry,
    like if there was a list of mixed sled-agent and crucible operations to
    work through, we'd have this issue again.

--- agent-client/src/lib.rs
+++ agent-client/src/lib.rs
@@ -6,3 +6,20 @@ generate_api!(
     spec = "../openapi/crucible-agent.json",
     derives = [schemars::JsonSchema],
 );
+
+// `omicron-common` defines some helpful retry operators for operations through
+// Progenitor clients.  Re-export them here to avoid a subtle versioning issue
+// it is otherwise easy to introduce: all uses of a `ProgenitorOperationRetry`
+// (and its Error) must agree on a `progenitor-client` version. In Omicron all
+// clients trivially agree due to the shared Cargo.toml, but Crucible's
+// Progenitor may reasonably be ahead or behind Omicron.
+//
+// We re-export `omicron-common` types here so that Omicron (and other
+// mixed-client consumers!) may have distinct `ProgenitorOperationRetry` names
+// for clients who may have differing Progenitor versions.
+pub mod progenitor_operation_retry {
+    pub use omicron_common::progenitor_operation_retry::{
+        ProgenitorOperationRetry,
+        ProgenitorOperationRetryError,
+    };
+}
