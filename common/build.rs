65ca41e821ef53ec9c28909357f23e3348169e4f
John Gallagher <john@oxidecomputer.com>
2025-09-17 16:55:30 -0400

    update to latest `vergen` (#1770)

--- common/build.rs
+++ common/build.rs
@@ -3,15 +3,24 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 // Copyright 2023 Oxide Computer Company
-use vergen::EmitBuilder;
+use vergen::CargoBuilder;
+use vergen::Emitter;
+use vergen::RustcBuilder;
+use vergen_git2::Git2Builder;
 
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
 
-    EmitBuilder::builder()
-        .all_cargo()
-        .all_git()
-        .all_rustc()
+    let cargo = CargoBuilder::all_cargo().expect("valid builder");
+    let rustc = RustcBuilder::all_rustc().expect("valid builder");
+    let git = Git2Builder::all_git().expect("valid builder");
+    Emitter::default()
+        .add_instructions(&cargo)
+        .expect("valid instructions")
+        .add_instructions(&git)
+        .expect("valid instructions")
+        .add_instructions(&rustc)
+        .expect("valid instructions")
         .emit()
-        .unwrap();
+        .expect("emitted version info");
 }
