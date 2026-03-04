3dc8312cddabd6300f10bf0f3de868129c323cf6
Rain <rain@oxide.computer>
2026-03-03 17:15:29 -0800

    [spr] initial version
    Created using spr 1.3.6-beta.1

---
+++ downstairs-types/versions/src/impls/mod.rs
@@ -0,0 +1,6 @@
+// Copyright 2026 Oxide Computer Company
+
+//! Functional code for the latest versions of types.
+
+// Currently a placeholder. Remove this comment when functional code is added in
+// a submodule.
